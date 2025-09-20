0fc5be48aa4cbe2ae7489d4ddf4fa085506d195e
waadhmd <waad.hamoud94@gmail.com>
2025-09-20 16:07:59 +0200

    feat: add form to create new blog posts

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
+from werkzeug.utils import redirect
 
 POSTS_FILE_NAME = 'posts.json'
 app = Flask(__name__)
@@ -11,10 +12,31 @@ def load_posts():
     except FileNotFoundError:
         return []
 
+def save_posts(post):
+    with open(POSTS_FILE_NAME,'w',encoding='UTF-8') as file:
+        json.dump(post,file,ensure_ascii=False, indent=2)
+
 @app.route('/')
 def index():
     blog_posts = load_posts()
     return render_template('index.html',posts=blog_posts )
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    blog_posts = load_posts()
+    if request.method == 'POST':
+        posts = load_posts()
+        new_post = {
+            "id": len(posts) + 1,
+            "title": request.form.get('title'),
+            "content": request.form.get('content'),
+            "author": request.form.get('author')
+        }
+        blog_posts.append(new_post)
+        save_posts(blog_posts)
+        return redirect(url_for('index'))
+
+    return render_template('add_post.html')
 
 
 if __name__ == '__main__':
